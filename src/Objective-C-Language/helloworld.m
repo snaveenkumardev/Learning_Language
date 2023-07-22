71d51969ef89655bbc7cd90a168bc0b1699bdf63
snaveenkumardev <snaveenkumardev@gmail.com>
2023-07-22 23:34:06 +0530

    Hello world

---
+++ src/Objective-C-Language/helloworld.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h> // Preprocessor command
+
+// Local Environment Setup for run the objective-c programs: https://www.tutorialspoint.com/objective_c/objective_c_environment_setup.htm
+
+// Compiler Command for compile "helloworld.m" source file: gcc `gnustep-config --objc-flags` -L /GNUstep/System/Library/Libraries helloworld.m -o helloworld -lgnustep-base -lobjc
+// After Compilation execute the "helloworld.exe" executable file command: /helloworld.exe
+
+int main() {
+    NSLog(@"Hello World!");
+    return 0;
+}
